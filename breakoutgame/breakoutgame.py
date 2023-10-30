bf0f60120c63d4710a675fd232730b5e8b438773
evankoh <kwh.evan@gmail.com>
2023-10-30 22:18:24 +0800

    updated with brick class

--- breakoutgame/breakoutgame.py
+++ breakoutgame/breakoutgame.py
@@ -55,6 +55,25 @@ class Paddle(GameObject):
             super(Paddle,self).move(offset,0)
             if self.ball is not None:
                 self.ball.move(offset,0)
+
+#Define Brick class to create bricks in game
+class Brick(GameObject):
+    COLORS={1:'#999999',2:'#555555',3:'222222'}
+    
+    def __init__(self,canvas,x,y,hits):
+        self.width=75
+        self.height=20
+        self.hits=hits
+        color=Brick.COLORS[hits]
+        item=canvas.create_rectangle(x-self.width/2,y-self.height/2,x+self.width/2,y+self.height/2,fill=color,tags='brick')
+        super(Brick,self).__init__(canvas,item)
+    
+    def hit(self):
+        self.hits=-1
+        if self.hits==0:
+            self.delete()
+        else:
+            self.canvas.itemconfig(self.item,fill=Brick.COLORS[self.hits])
     
 
 if __name__=='__main__':
