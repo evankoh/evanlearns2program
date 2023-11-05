17fcae9b1b387d91d96a42a7d562e931d6ebc27d
Evan <kwh.evan@gmail.com>
2023-11-05 09:35:20 +0800

    overloading and overriding, f-string, method, parameter, function

---
+++ test.py
@@ -0,0 +1,6 @@
+name="Alice"
+age=30
+height=5.8
+#print(f"My name is {name}. I am {age} years old, and I am {height} feet tall.")
+
+print("My name is %s. I am %d years old, and I am %r feet tall."%(name,age,height))
