641c690d000e22fc49efcfeec22aeec9c1425ee5
evankoh <kwh.evan@gmail.com>
2023-11-16 23:53:22 +0800

    studied until ex26 from book

--- LearnPythonTheHardWay/test.py
+++ LearnPythonTheHardWay/test.py
@@ -1,36 +1,41 @@
-from sys import argv
+txt = "welcome to the jungle"
+x=txt.split()
+print(sorted(x))
 
-script, filename = argv
 
-print(f"We're going to erase {filename}")
-print("If you don't want that, hit CTRL-C (^C).")
-print("If you want that, hit RETURN.")
+#from sys import argv
 
-input("?")
+#script, filename = argv
 
-print("Opening the file...")
+#print(f"We're going to erase {filename}")
+#print("If you don't want that, hit CTRL-C (^C).")
+#print("If you want that, hit RETURN.")
 
-with open(filename,'r+') as target:
-    print("Truncating the file. Goodbye!")
+#input("?")
+
+#print("Opening the file...")
+
+#with open(filename,'r+') as target:
+#    print("Truncating the file. Goodbye!")
 #    print(target.read())
-    target.truncate()
+#    target.truncate()
     
-    print("Now I'm going to ask you for three lines.")
+#    print("Now I'm going to ask you for three lines.")
     
-    line1=input("line 1:")
-    line2=input("line 2:")
-    line3=input("line 3:")
+#    line1=input("line 1:")
+#    line2=input("line 2:")
+#    line3=input("line 3:")
     
-    print("I'm going to write these to the file.")
+#    print("I'm going to write these to the file.")
     
-    target.write(f"{line1}\n{line2}\n{line3}\n")
+#    target.write(f"{line1}\n{line2}\n{line3}\n")
 #    target.write("\n")
 #    target.write(line2)
 #    target.write("\n")
 #    target.write(line3)
 #    target.write("\n")
     
-    print("And finally, we close it.")
+#    print("And finally, we close it.")
 
 
 #txt=open(filename)
