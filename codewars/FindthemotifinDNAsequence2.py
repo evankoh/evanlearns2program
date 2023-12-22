37c2418b947591425796546a83a3b722e575f5ba
evankoh <kwh.evan@gmail.com>
2023-12-23 00:09:42 +0800

    solved dna question

---
+++ codewars/FindthemotifinDNAsequence2.py
@@ -0,0 +1,19 @@
+def motif_locator(sequence, motif):
+    if motif not in sequence:
+        return []
+    else:
+        seq_lst=[]
+        counter_index=0
+        counter=0
+        counter2=0
+        for i in sequence:
+            if counter<=(len(sequence)-len(motif)+1):
+                checker=sequence[counter:(counter+len(motif))]
+                print(checker)
+                if checker==motif:
+                    seq_lst.append(counter+1)
+            counter+=1
+        print(seq_lst)
+        return seq_lst
+
+motif_locator("TTCCGGAACC", "CC")
