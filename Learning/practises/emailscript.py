3f19ef3bad686bfbb8fe29923581f20a12cd2497
Evan <kwh.evan@gmail.com>
2024-03-24 17:11:09 +0800

    python session

--- Learning/practises/emailscript.py
+++ Learning/practises/emailscript.py
@@ -1,16 +1,21 @@
-"""import smtplib
+import smtplib
 
 def send_email(from_addr,to_addrs, email_subject, email_body):
-  for i in to_addrs:
-    SMTP.sendmail(from_addr,i,email_subject,email_body)
+    server=smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('kwh.evan@gmail.com','')
+    message=f"{email_subject}\n{email_body}"
+    for i in to_addrs:
+        server.sendmail(from_addr,i,message)
+    server.quit()
 
-server=smtplib.SMTP('kwh.evan@gmail.com', 587)
+from_addr='kwh.evan@gmail.com'
 to_addrs=['evanosekai@gmail.com','buklau450@gmail.com']
 email_subject=input('Please input email subject:')
 email_body=input('Please input email body:')
-send_email(server,to_addrs,email_subject,email_body)"""
+send_email(from_addr,to_addrs,email_subject,email_body)
 
-import smtplib
+'''import smtplib
 
 def send_email(from_addr, to_addrs, email_subject, email_body):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -25,4 +30,4 @@ from_addr = 'your_email@gmail.com'
 to_addrs = ['evanosekai@gmail.com', 'buklau450@gmail.com']
 email_subject = input('Please input email subject:')
 email_body = input('Please input email body:')
-send_email(from_addr, to_addrs, email_subject, email_body)
+send_email(from_addr, to_addrs, email_subject, email_body)'''
